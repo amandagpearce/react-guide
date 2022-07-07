d210075aa223f4fc1bc5dbc1f36c11153791e6ae
amandagpearce <amandagpearce@gmail.com>
2022-07-07 17:37:18 -0300

    dynamic styling for chart bars

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const ChartBar = (props) => {
+    let barFilHeight = '0%';
+
+    if (props.max > 0) {
+        barFilHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
+    return (
+        <div className="chart-bar">
+            <div className="chat-bar__inner">
+                <div className="chart-bar__fill" style={{height: barFilHeight}}></div> 
+            </div>
+            <div className="chart-bar__label">{props.label}</div>
+        </div>
+    );
+};
+
+export default ChartBar;
